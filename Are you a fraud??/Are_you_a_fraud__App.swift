1178c17cee1f14d2129015f145ba16fac8bacff8
Du Yuhan <yuhan.du@gmail.com>
2023-02-17 16:43:47 +0800

    Initial Commit

---
+++ Are you a fraud??/Are_you_a_fraud__App.swift
@@ -0,0 +1,17 @@
+//
+//  Are_you_a_fraud__App.swift
+//  Are you a fraud??
+//
+//  Created by Yuhan Du on 17/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Are_you_a_fraud__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Are you a fraud??UITests/Are_you_a_fraud__UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Are_you_a_fraud__UITestsLaunchTests.swift
+//  Are you a fraud??UITests
+//
+//  Created by Yuhan Du on 17/2/23.
+//
+
+import XCTest
+
+final class Are_you_a_fraud__UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
